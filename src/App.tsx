3471fbb41fe4122bd92a5e6a00663ef1894daa58
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-05 18:18:26 +0500

    Create version of App to deploy on Surge
    1:- Created
    	a:- src\component\Header.tsx for title of the app.
    	b:- src\component\Body.tsx for body of the app.
    	c:- src\component\Footer.tsx for footer of the app.
    	d:- .github\workflows\main.yml to deploy app on the surge link.
    2:- Updated
    	e:- src\App.tsx to add components in the App.
    	f:- src\App.css for styling of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+// Imports
+// React Imports
 import React from 'react';
+// Component Imports
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Body } from './components/Body';
+// Styles Imports
 import './App.css';
 
 function App() {
   return (
     <div>
-      Expense Tracker PWA
+      <Header/>
+      <div className="container">
+        <Body/>
+      </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+// React Imports
+import React from 'react';
+
+export const Header = () => {
+    return (
+        // Header Container
+        <div className="header">
+            <h2>
+                Expense Tracker
+            </h2>
+        </div>
+    )
+}
